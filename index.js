8ed50b1f1e3e14d33a30cb7acd73ee749a799529
skStavir <santhosh.krishna@stavir.com>
2024-12-25 15:26:37 +0530

    upload api

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const messageRoutes = require('./routes/message');
 const feeRecords = require('./routes/feeRecord');
 const assignments = require('./routes/assignment');
 const login = require('./routes/login');
+const uploads = require('./routes/upload');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
 
@@ -30,6 +31,7 @@ index.use('/messages', messageRoutes);
 index.use('/fee-records', feeRecords);
 index.use('/assignments', assignments);
 index.use('/login', login)
+index.use('/uploads', uploads)
 
 index.get('/', (req, res) => {
     res.send('{"name":"Private tutor apis"}');
---
+++ routes/upload.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
+const router = express.Router();
+
+// Configure AWS SDK
+// AWS.config.update({
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//     region: process.env.AWS_REGION
+// });
+
+// const s3 = new AWS.S3();
+
+// Configure multer storage to use S3
+// const upload = multer({
+//     storage: multerS3({
+//         s3: s3,
+//         bucket: process.env.S3_BUCKET_NAME,
+//         acl: 'public-read',
+//         metadata: function (req, file, cb) {
+//             cb(null, { fieldName: file.fieldname });
+//         },
+//         key: function (req, file, cb) {
+//             cb(null, Date.now().toString() + path.extname(file.originalname));
+//         }
+//     })
+// });
+
+// Upload route actual
+// router.post('/upload', upload.single('file'), (req, res) => {
+//     if (!req.file) {
+//         return res.status(400).send('No file uploaded.');
+//     }
+//     const fileUri = req.file.location;
+//     res.json({ uri: fileUri });
+// });
+
+// Upload route dummy
+router.post('/', (req, res) => {
+    console.log('file uploaded');
+    res.json({ uri: 'https://example-bucket.s3.us-west-2.amazonaws.com/sample-file.png' });
+});
+
+module.exports = router;
