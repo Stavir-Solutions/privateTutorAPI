c32a526c01178a4b4756e912d250ede12ea056b7
Keerthi-creator-hash <keerthi@stavir.com>
2025-02-25 11:31:14 +0530

    modification in signup and message

--- src/main/routes/signup.js
+++ src/main/routes/signup.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const {buildSuccessResponse, buildErrorMessage} = require('./responseUtils');
 const {create} = require('../services/teacherService');
+const {createStudent} = require('../services/studentService');
+
 
 const router = express.Router();
 const Joi = require('joi');
@@ -29,6 +31,30 @@ const teacherSchema = Joi.object({
 }).unknown(false);
 
 
+const studentSchema = Joi.object({
+    firstName: Joi.string().max(50).optional(),
+    lastName: Joi.string().max(50).optional(),
+    userName: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().min(6).max(20).pattern(new RegExp('^[a-zA-Z0-9]{6,20}$')).required(),
+    email: Joi.string().email().optional(),
+    age: Joi.number().integer().optional(),
+    addressLine1: Joi.string().optional(),
+    addressCity: Joi.string().optional(),
+    addressState: Joi.string().optional(),
+    pinCode: Joi.number().optional(),
+    profilePicUrl: Joi.string().uri().optional(),
+    gender: Joi.string().valid('male', 'female', 'do not disclose').optional(),
+    parent1Name: Joi.string().optional(),
+    parent1Phone: Joi.string().pattern(/^[0-9]{10}$/).required(),
+    parent1Email: Joi.string().email().optional(),
+    parent2Name: Joi.string().optional(),
+    parent2Phone: Joi.string().pattern(/^[0-9]{10}$/).optional(),
+    parent2Email: Joi.string().email().optional(),
+    batches: Joi.array().items(Joi.string()).optional(),
+
+}).unknown(false);
+
+
 router.post('/teachers', async (req, res) => {
     console.log(JSON.stringify(req.body))
     const {error} = teacherSchema.validate(req.body);
@@ -42,5 +68,18 @@ router.post('/teachers', async (req, res) => {
     console.log('created teacher {}', teacherId);
 });
 
+
+router.post('/students', async (req, res) => {
+    console.log(JSON.stringify(req.body))
+    const {error} = studentSchema.validate(req.body);
+    if (error) {
+        console.log('error: {}', error);
+        return buildErrorMessage(res, 400, error.details[0].message);
+    }
+    console.log('creating student {}', req.body);
+    let studentId = await createStudent(req.body)
+    buildSuccessResponse(res, 200, {token: await generateToken(buildTeacherPayload(req.body))  });
+    console.log('created student {}', studentId);
+});
 
 module.exports = router;
