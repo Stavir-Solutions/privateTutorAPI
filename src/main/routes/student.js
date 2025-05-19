0a2e484343b619914506208958e64a46516f68ff
Keerthi-creator-hash <keerthi@stavir.com>
2025-05-19 09:50:28 +0530

    added new api for timeline (#115)

--- src/main/routes/student.js
+++ src/main/routes/student.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const {buildSuccessResponse, buildErrorMessage} = require('./responseUtils');
-const {
+const { buildSuccessResponse, buildErrorMessage } = require('./responseUtils');
+const {createStudent,
     getStudentById,
     getAll,
     deleteById,
     updateStudent,
     getByBatchId,
-    createStudent,
     getStudentByIdWithBatchName,
-    getStudentByUserName
-} = require('../services/studentService');
+    getStudentByUserName,
+    getTimelineData,} = require('../services/studentService');
 
 
 const router = express.Router();
@@ -18,8 +17,8 @@ const authMiddleware = require("../middleware/authMiddleware");
 router.use(express.json());
 
 const studentSchema = Joi.object({
-    firstName: Joi.string().max(50).optional().messages({'string.max': 'First name should not exceed 50 characters.'}),
-    lastName: Joi.string().max(50).optional().messages({'string.max': 'Last name should not exceed 50 characters.'}),
+    firstName: Joi.string().max(50).optional().messages({ 'string.max': 'First name should not exceed 50 characters.' }),
+    lastName: Joi.string().max(50).optional().messages({ 'string.max': 'Last name should not exceed 50 characters.' }),
     userName: Joi.string().alphanum().min(3).max(30).required().messages({
         'string.alphanum': 'Username must be alphanumeric.',
         'string.min': 'Username must be at least 3 characters long.',
@@ -30,37 +29,37 @@ const studentSchema = Joi.object({
         'string.max': 'Password must not exceed 20 characters.',
         'string.pattern.name': 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
     }),
-    email: Joi.string().email().required().messages({'string.email': 'Email must be a valid email address.'}),
+    email: Joi.string().email().required().messages({ 'string.email': 'Email must be a valid email address.' }),
     age: Joi.number().integer().optional().messages({
         'number.base': 'Age must be a number.',
     }),
-    addressLine1: Joi.string().optional().messages({'string.base': 'Address line 1 must be a string.'}),
-    addressCity: Joi.string().optional().messages({'string.base': 'City must be a string.'}),
-    addressState: Joi.string().optional().messages({'string.base': 'State must be a string.'}),
-    pinCode: Joi.number().optional().messages({'number.base': 'Pin code must be a number.'}),
-    profilePicUrl: Joi.string().uri().optional().messages({'string.uri': 'Profile picture URL must be a valid URI.'}),
+    addressLine1: Joi.string().optional().messages({ 'string.base': 'Address line 1 must be a string.' }),
+    addressCity: Joi.string().optional().messages({ 'string.base': 'City must be a string.' }),
+    addressState: Joi.string().optional().messages({ 'string.base': 'State must be a string.' }),
+    pinCode: Joi.number().optional().messages({ 'number.base': 'Pin code must be a number.' }),
+    profilePicUrl: Joi.string().uri().optional().messages({ 'string.uri': 'Profile picture URL must be a valid URI.' }),
     gender: Joi.string().valid('male', 'female', 'do not disclose').optional().messages({
         'string.base': 'gender must be a string.',
         'any.only': 'Gender must be : male, female, or do not disclose.',
     }),
-    parent1Name: Joi.string().optional().messages({'string.base': 'Parent 1 name must be a string.'}),
+    parent1Name: Joi.string().optional().messages({ 'string.base': 'Parent 1 name must be a string.' }),
     parent1Phone: Joi.string().pattern(/^[0-9]{10}$/).required().messages({
 
         'string.pattern.base': 'Parent 1 phone number must be a valid 10-digit number.'
     }),
-    parent1Email: Joi.string().email().optional().messages({'string.email': 'Parent 1 email must be a valid email address.'}),
-    parent2Name: Joi.string().optional().messages({'string.base': 'Parent 2 name must be a string.'}),
+    parent1Email: Joi.string().email().optional().messages({ 'string.email': 'Parent 1 email must be a valid email address.' }),
+    parent2Name: Joi.string().optional().messages({ 'string.base': 'Parent 2 name must be a string.' }),
     parent2Phone: Joi.string().pattern(/^[0-9]{10}$/).optional().messages({
         'string.pattern.base': 'Parent 2 phone number must be a valid 10-digit number.'
     }),
-    parent2Email: Joi.string().email().optional().messages({'string.email': 'Parent 2 email must be a valid email address.'}),
-    batches: Joi.array().items(Joi.string()).optional().messages({'array.base': 'Batches must be an array of strings and strings are uuid of batchId'}),
+    parent2Email: Joi.string().email().optional().messages({ 'string.email': 'Parent 2 email must be a valid email address.' }),
+    batches: Joi.array().items(Joi.string()).optional().messages({ 'array.base': 'Batches must be an array of strings and strings are uuid of batchId' }),
 
 }).unknown(false);
 
 const studentUpdateSchema = Joi.object({
-    firstName: Joi.string().max(50).optional().messages({'string.max': 'First name should not exceed 50 characters.'}),
-    lastName: Joi.string().max(50).optional().messages({'string.max': 'Last name should not exceed 50 characters.'}),
+    firstName: Joi.string().max(50).optional().messages({ 'string.max': 'First name should not exceed 50 characters.' }),
+    lastName: Joi.string().max(50).optional().messages({ 'string.max': 'Last name should not exceed 50 characters.' }),
     userName: Joi.string().alphanum().min(3).max(30).optional().messages({
         'string.alphanum': 'Username must be alphanumeric.',
         'string.min': 'Username must be at least 3 characters long.',
@@ -75,28 +74,28 @@ const studentUpdateSchema = Joi.object({
         'number.base': 'Age must be a number.',
         'number.integer': 'Age must be an integer.'
     }),
-    addressLine1: Joi.string().optional().messages({'string.base': 'Address line 1 must be a string.'}),
-    addressCity: Joi.string().optional().messages({'string.base': 'City must be a string.'}),
-    addressState: Joi.string().optional().messages({'string.base': 'State must be a string.'}),
-    pinCode: Joi.number().optional().messages({'number.base': 'Pin code must be a number.'}),
-    profilePicUrl: Joi.string().uri().optional().messages({'string.uri': 'Profile picture URL must be a valid URI.'}),
+    addressLine1: Joi.string().optional().messages({ 'string.base': 'Address line 1 must be a string.' }),
+    addressCity: Joi.string().optional().messages({ 'string.base': 'City must be a string.' }),
+    addressState: Joi.string().optional().messages({ 'string.base': 'State must be a string.' }),
+    pinCode: Joi.number().optional().messages({ 'number.base': 'Pin code must be a number.' }),
+    profilePicUrl: Joi.string().uri().optional().messages({ 'string.uri': 'Profile picture URL must be a valid URI.' }),
     gender: Joi.string().valid('male', 'female', 'do not disclose').optional().messages({
         'string.base': 'geneder must be a string.',
         'any.only': 'Gender must be : male, female, or do not disclose.',
     }),
 
 
-    parent1Name: Joi.string().optional().messages({'string.base': 'Parent 1 name must be a string.'}),
+    parent1Name: Joi.string().optional().messages({ 'string.base': 'Parent 1 name must be a string.' }),
     parent1Phone: Joi.string().pattern(/^[0-9]{10}$/).optional().messages({
         'string.pattern.base': 'Parent 1 phone number must be a valid 10-digit number.'
     }),
-    parent1Email: Joi.string().email().optional().messages({'string.email': 'Parent 1 email must be a valid email address.'}),
-    parent2Name: Joi.string().optional().messages({'string.base': 'Parent 2 name must be a string.'}),
+    parent1Email: Joi.string().email().optional().messages({ 'string.email': 'Parent 1 email must be a valid email address.' }),
+    parent2Name: Joi.string().optional().messages({ 'string.base': 'Parent 2 name must be a string.' }),
     parent2Phone: Joi.string().pattern(/^[0-9]{10}$/).optional().messages({
         'string.pattern.base': 'Parent 2 phone number must be a valid 10-digit number.'
     }),
-    parent2Email: Joi.string().email().optional().messages({'string.email': 'Parent 2 email must be a valid email address.'}),
-    batches: Joi.array().items(Joi.string()).optional().messages({'array.base': 'Batches must be an array of strings and strings are uuid of batchId'}),
+    parent2Email: Joi.string().email().optional().messages({ 'string.email': 'Parent 2 email must be a valid email address.' }),
+    batches: Joi.array().items(Joi.string()).optional().messages({ 'array.base': 'Batches must be an array of strings and strings are uuid of batchId' }),
 }).or(
     'firstName', 'lastName', 'age', 'addressLine1', 'addressCity', 'addressState', 'pinCode',
     'profilePicUrl', 'gender', 'parent1Name', 'parent1Phone', 'parent1Email', 'parent2Name',
@@ -124,7 +123,7 @@ router.get('/batch/:batchId', authMiddleware, async (req, res) => {
 
 router.post('/', authMiddleware, async (req, res) => {
     console.log(JSON.stringify(req.body))
-    const {error} = studentSchema.validate(req.body);
+    const { error } = studentSchema.validate(req.body);
     if (error) {
         console.log('error: {}', error);
         return buildErrorMessage(res, 400, error.details[0].message);
@@ -135,7 +134,7 @@ router.post('/', authMiddleware, async (req, res) => {
         buildSuccessResponse(res, 200, '{"id":"' + studentId + '"}')
         console.log('created student {}', studentId);
     } catch
-        (err) {
+    (err) {
         {
             if (err.message === 'userName already exists') {
                 return buildErrorMessage(res, 409, 'Username already exists');
@@ -148,7 +147,7 @@ router.post('/', authMiddleware, async (req, res) => {
 
 /* API to update the student */
 router.put('/:id', authMiddleware, async (req, res) => {
-    const {error} = studentUpdateSchema.validate(req.body);
+    const { error } = studentUpdateSchema.validate(req.body);
     if (error) {
         console.log('Validation error:', error);
         return buildErrorMessage(res, 400, error.details[0].message);
@@ -178,21 +177,32 @@ router.delete('/:id', authMiddleware, async (req, res) => {
 
 router.get('/userName/:userName', async (req, res) => {
 
-    const {userName} = req.params;
+    const { userName } = req.params;
 
     try {
         const user = await getStudentByUserName(userName);
 
         if (user) {
-            return res.status(200).json({message: ` ${userName} exist`});
+            return res.status(200).json({ message: ` ${userName} exist` });
         }
-        return res.status(404).json({message: ` ${userName} does not exist`});
+        return res.status(404).json({ message: ` ${userName} does not exist` });
 
     } catch (err) {
         console.error('Error checking Student userName:', err);
-        return res.status(500).json({error: 'Internal server error'});
+        return res.status(500).json({ error: 'Internal server error' });
     }
 });
 
+router.get('/:studentId/batch/:batchId/timeline',authMiddleware, (req, res) => {
+    const { studentId, batchId } = req.params;
+    const timeline = getTimelineData(studentId, batchId);
+    const response = {
+      studentId,
+      batchId,
+      timeline
+    };
+  
+    buildSuccessResponse(res, 200, response);
+  });
 
 module.exports = router;
